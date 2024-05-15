5da1b739f7d22f9b1c92254e6816e55962f67c47
tlun0911 <tlun0911@gmail.com>
2024-05-15 12:21:13 -0400

    New commit

--- new_app/settings.py
+++ new_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-g@903e)f5#^s#e4dxu76a!n#n616x(ob3p$t%r^d#*h7g%rlh#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backend',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'new_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mealplanner',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Nashnash#1',
+        'PORT': '3306',
     }
 }
 
--- new_app/urls.py
+++ new_app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+app_name='new_app'
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
